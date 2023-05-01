82e9f7715b3e3e3cf8888b4d7b0ee05650bfe5fd
Abu-Taieb <taieb.contact.bd@gmail.com>
2023-05-01 21:20:43 +0600

    Main Content Data Loaded

---
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Card, Image } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const NewsCard = ({ news }) => {
+  const { _id, title, details, image_url, author } = news;
+  return (
+    <Card className=" mb-4">
+      <Card.Header className="d-flex">
+        <div className="">
+          <Image className="img-fluid w-25" src={author.img} roundedCircle />
+        </div>
+        <div className="">
+          {author.name}
+          {author.published_date}
+        </div>
+      </Card.Header>
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <img className="img-fluid" src={image_url} alt="" />
+        <Card.Text>
+            {details.length < 245 ? <>{details}</> : <>{details.slice(0, 246)}... <Link className="text-decoration-none fw-bold text-info" to={`/news/${_id}`}> Read more</Link> </>
+            }
+        </Card.Text>
+      </Card.Body>
+      <Card.Footer className="text-muted">2 days ago</Card.Footer>
+    </Card>
+  );
+};
+
+export default NewsCard;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import Home from "../pages/Home/Home/Home";
+import Category from "../pages/Home/Category/Category";
+import NewsLayouts from "../layouts/NewsLayouts";
+import News from "../pages/Newses/News/News";
 
 const router = createBrowserRouter([
     {
@@ -10,6 +13,21 @@ const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>
+            },
+            {
+                path: '/category/:id',
+                element: <Category></Category>,
+                loader: ({params}) => fetch(`http://localhost:5000/catagories/${params.id}`)
+            },
+        ]
+    },
+    {
+        path: 'news',
+        element: <NewsLayouts></NewsLayouts>,
+        children: [
+            {
+                path:'news/:id',
+                element: <News></News>
             }
         ]
     }

5ea59f4279844a33139e78fc914ffff3c940fa2c
Abu-Taieb <taieb.contact.bd@gmail.com>
2023-05-01 10:48:35 +0600

    route setup

---
+++ src/pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p className='text-center text-primary'>copyright copy@; reserved of my news</p>
+        </div>
+    );
+};
+
+export default Footer;

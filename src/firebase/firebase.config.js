7fd10b1a499d690a80c2feeae3d4e487524b9d85
Abu-Taieb <taieb.contact.bd@gmail.com>
2023-05-01 09:14:31 +0600

    Start News Paper Project

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCnO29uTYar2TRjNV1_r9HMnXetI-QEKh4",
+  authDomain: "news-paper-ff7bd.firebaseapp.com",
+  projectId: "news-paper-ff7bd",
+  storageBucket: "news-paper-ff7bd.appspot.com",
+  messagingSenderId: "415811590550",
+  appId: "1:415811590550:web:906a5270f2570f1e24a5b7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;

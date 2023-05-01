8ae0a49d118d6934172bad5f7c901e87317305e6
Abu-Taieb <taieb.contact.bd@gmail.com>
2023-05-01 12:06:28 +0600

    Header Desing Complete

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <p className='text-center text-primary'>copyright copy@; reserved of my news</p>
+            <p className='text-center text-primary'>copyright &copy; reserved of my news</p>
         </div>
     );
 };
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React from "react";
+import logo from "../../../assets/logo.png";
+import moment from "moment";
+import Marquee from "react-fast-marquee";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendarDays } from "@fortawesome/free-solid-svg-icons";
+import { Button, Container } from "react-bootstrap";
 
 const Header = () => {
-    return (
-        <div>
-            <h2>This is Header</h2>
-        </div>
-    );
+  return (
+    <Container>
+      <div className="text-center">
+        <img src={logo} alt="" />
+        <p className="text-secondary">
+          <small>Journalism Without Fear or Favour</small>
+        </p>
+        <p className="text-">
+          <span>
+            <FontAwesomeIcon icon={faCalendarDays} />
+          </span>{" "}
+          {moment().format("dddd, MMMM D, YYYY")}
+        </p>
+      </div>
+      <div className="d-flex bg-warning rounded">
+        <Button className="" variant="danger">Latest</Button>
+        <Marquee className="text-danger" speed={50}>
+          I can be a React component, multiple React components, or just some
+          text. I can be a React component, multiple React components, or just some
+          text.
+        </Marquee>
+      </div>
+
+
+
+
+
+    </Container>
+  );
 };
 
 export default Header;

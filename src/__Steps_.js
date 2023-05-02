48f0d2a61051c728f0f9b69f748f6a3864ff3cc2
Abu-Taieb <taieb.contact.bd@gmail.com>
2023-05-02 08:35:52 +0600

    Setup File Include

---
+++ src/__Steps_.js
@@ -0,0 +1,28 @@
+/*
+
+React Project Setup Steps
+
+1. go to -> React Router ->
+ * command git -> npm create vite@latest name-of-your-project -- --template react
+ * cd project
+ * command git -> npm install react-router-dom localforage match-sorter sort-by
+
+2. go to -> Firebase -> add project -> enter project name ->
+   Disable google analytics -> Create Project -> Continue ->
+   web -> Register App Name ->
+ * command git -> npm i firebase
+ * copy to apiKey -> Continue to console -> Authentication -> Get started -> Sign-in    providers -> Email & Password enable / google enable
+
+3. npm install react-bootstrap bootstrap
+ * Delete all default css
+ * add bootstrap css file to main.jsx from react bootstrap doc
+
+4. npm install moment
+
+5. npm install express
+
+6. npm install -g nodemon
+
+7. npm install cors
+
+*/
